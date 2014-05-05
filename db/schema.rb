21586d88921a219b39471a7a48efd387aed8eced
pnilan <nilan.patrick@gmail.com>
2014-05-05 14:58:29 -0700

    Added user session functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140503041301) do
+ActiveRecord::Schema.define(version: 20140505194527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,9 +37,11 @@ ActiveRecord::Schema.define(version: 20140503041301) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -21,14 +21,23 @@ describe "User pages" do
 		describe "with valid information" do
 			before do
 				fill_in "Username",			with: "example_user"
-				fill_in "Name",				with: "Example User"
+				fill_in "Full name",		with: "Example User"
 				fill_in "Email",			with: "example@user.com"
 				fill_in "Password",			with: "foobar"
-				fill_in "Username",			with: "foobar"
+				fill_in "Confirmation",		with: "foobar"
 			end
 
 			it "should create a user" do
-				expect { click_button submit}.to change(User, :count).by(1)
+				expect { click_button submit }.to change(User, :count).by(1)
+			end
+
+			describe "after saving the user" do
+				before { click_button submit }
+				let(:user) { User.find_by(email: 'user@example.com') }
+
+				it { should have_link('Sign out') }
+				it { should have_title(full_title('')) }
+				it { should have_selector('div.alert.alert-success', text: "SecondWind!") }
 			end
 		end
     end
