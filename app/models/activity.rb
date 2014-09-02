e5ea67c43bc569a337753b84e61ab89e211ee628
pnilan <nilan.patrick@gmail.com>
2014-09-02 08:27:07 -0700

    updated todo.txt

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -5,8 +5,6 @@ class Activity < ActiveRecord::Base
 	extend SimpleCalendar
 	has_calendar attribute: "date"
 
-
-	
 	before_save :parse_duration, :parse_pace
 
 	belongs_to :user
@@ -30,7 +28,7 @@ class Activity < ActiveRecord::Base
 
 	def self.distance_today(current_user)
 		distance = 0
-		activities = Activity.where('user_id= ? AND date > ?', current_user.id, Chronic.parse('yesterday'))
+		activities = Activity.where('user_id= ? AND date = ?', current_user.id, Date.today)
 
 		activities.each do |activity|
 			if activity.distance != nil
@@ -42,7 +40,18 @@ class Activity < ActiveRecord::Base
 
 	def self.distance_this_week(current_user)
 		distance = 0
-		activities = Activity.where('user_id= ? AND date >= ?', current_user.id, Chronic.parse('last monday'))
+		if Date.today.wday != 1
+			activities = Activity.where('user_id= ? AND date >= ? AND date < ?', current_user.id, Chronic.parse('last monday'), Chronic.parse('next monday'))
+		else
+			activities = Activity.where('user_id= ? AND date >= ? AND date < ?', current_user.id, Date.today, Chronic.parse('next monday'))
+		end
+
+		distance = activities.sum(:distance).round(1)
+	end
+
+	def self.distance_this_month(current_user)
+		distance = 0
+		activities = Activity.where('user_id= ? AND date >= ? AND date < ?', current_user.id, Chronic.parse('the first of this month'), Chronic.parse('the first of this month') + 1.month)
 
 		activities.each do |activity|
 			if activity.distance != nil
@@ -52,16 +61,20 @@ class Activity < ActiveRecord::Base
 		distance.round(1)
 	end
 
-	def self.distance_this_month(current_user)
-		distance = 0
-		activities = Activity.where('user_id= ? AND date >= ?', current_user.id, Chronic.parse('the first of this month'))
+	def self.this_week
+		days = 1..7
+		distances = []
 
-		activities.each do |activity|
-			if activity.distance != nil
-				distance += activity.distance
+		days.each do |day|
+			if day < Date.today.wday
+				distances << where('date = ?', Chronic.parse('last #{Date::DAYNAMES[day]}')).sum(:distance).round(1)
+			elsif day > Date.today.wday
+				distances << where('date = ?', Chronic.parse(Date::DAYNAMES[day])).sum(:distance).round(1)
+			else
+				distances << where('date = ?', Date.today).sum(:distance).round(1)
 			end
 		end
-		distance.round(1)
+		distances
 	end
 
 	def chrono_duration
