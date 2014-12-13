93a889030e29b1f131b993c23bcce87480ea8e02
pnilan <nilan.patrick@gmail.com>
2014-12-13 09:08:18 -0800

    update deleted files

--- app/models/type.rb
+++
@@ -1,3 +0,0 @@
-class Type < ActiveRecord::Base
-	validates :name, presence: true
-end
--- db/migrate/20140527192407_remove_type_from_activities.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTypeFromActivities < ActiveRecord::Migration
-  def change
-  	remove_column :activities, :type
-  end
-end
